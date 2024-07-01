a6c866dda38201097ccefa1bb9f137b1e59c4a34
Trung-Hieu NGUYEN <44265287+hieu1999210@users.noreply.github.com>
2024-07-01 18:09:33 -0400

    fix ensemble.sh

--- ensemble.sh
+++ ensemble.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=$(pwd):$PYTHONPATH
 python spine/ensemble_classifier.py \
-  --config-file ./spine/configs/densenet121_dgx3.yaml \
+  --config-file ./spine/configs/densenet121.yaml \
   --output-dir ./outputs/ensemble_densenet
